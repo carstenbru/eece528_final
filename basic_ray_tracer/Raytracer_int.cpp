b62de5a1533b08f39ea4af50d31092f14bdcacb4
Carsten Bruns <carst.bruns@gmx.de>
2015-12-10 15:42:20 -0800

    (almost) no floats anymore! OpenCL broken atm

--- basic_ray_tracer/Raytracer_int.cpp
+++ basic_ray_tracer/Raytracer_int.cpp
@@ -38,12 +38,6 @@ using namespace std;
 
 #define UNSIGNED_MAX 0xFFFFFFFF
 
-/*
- float mix(const float &a, const float &b, const float &mix) {
- return b * mix + a * (1 - mix);
- }*/
-float mix(const float &a, const float &b, const float &mix);
-
 Color Raytracer_int::trace(const Vec3i &rayorig, const Vec3i &raydir,
 		const int &depth) {
 	unsigned int tnear = UNSIGNED_MAX;
@@ -64,31 +58,31 @@ Color Raytracer_int::trace(const Vec3i &rayorig, const Vec3i &raydir,
 	if (!object)
 		return generateColorI(3 * 256, 3 * 256, 3 * 256);
 
-	Color surfaceColor = { 0, 0, 0 };  // color of the ray/surface of the object intersected by the ray
-	Vec3i phit = mul(add(conv_fp(rayorig, SCENE_COORDINATE_PRECISION), raydir),
-			tnear);  // point of intersection
+	Color surfaceColor = object->surfaceColor;  // color of the ray/surface of the object intersected by the ray
+	Vec3i phit = mul(add(rayorig, raydir), tnear);  // point of intersection
 	Vec3i nhit = sub(phit, conv_fp(object->center, SCENE_COORDINATE_PRECISION));  // normal at the intersection point
-
-	//return generateColor(0, 0, 0);
 
 	normalize(&nhit);  // normalize normal direction
 	// If the normal and the view direction are not opposite to each other
 	// reverse the normal direction. That also means we are inside the sphere so set
 	// the inside bool to true. Finally reverse the sign of IdotN which we want
 	// positive.
-	unsigned int bias = 65;	//1e-4;  // add some bias to the point from which we will be tracing
+	unsigned int bias = 65;  //1e-4;  // add some bias to the point from which we will be tracing
 	if (dot(raydir, nhit) > 0)
 		nhit = sub(generateVectorI(0, 0, 0), nhit);  //invert normal if necessary
 	if ((object->reflection > 0) && depth < MAX_RAY_DEPTH) {
-		float facingratio = -dot(raydir, nhit) / (float)FP_ONE / FP_ONE; //TODO
+		int facingratio = (-dot(raydir, nhit) >> FP_PRECISION);
 		// change the mix value to tweak the effect
-		float fresneleffect = mix(pow(1 - facingratio, 3), 1, 0.1);
+		unsigned int fresneleffect = ((unsigned int) (0.1 * FP_ONE)
+				+ (unsigned int) (0.9 * FP_ONE)
+						* (pow((FP_ONE - facingratio) / (float) FP_ONE, 3)));  //TODO pow in fixed-point
 		// compute reflection direction (not need to normalize because all vectors
 		// are already normalized)
-		Vec3i refldir = mul(sub(raydir, nhit), 2 * dot(raydir, nhit) >> FP_PRECISION);
+		Vec3i refldir = mul(sub(raydir, nhit),
+				2 * dot(raydir, nhit) >> FP_PRECISION);
 		normalize(&refldir);
 		Color reflection = trace(add(phit, mul(nhit, bias)), refldir, depth + 1);
-		surfaceColor = mul(object->surfaceColor, mul(reflection, fresneleffect*FP_ONE));
+		surfaceColor = mul(object->surfaceColor, mul(reflection, fresneleffect));
 	} else {
 		// it's a diffuse object, no need to raytrace any further
 		for (unsigned i = 0; i < objects.size(); ++i) {
@@ -125,19 +119,22 @@ Color Raytracer_int::trace(const Vec3i &rayorig, const Vec3i &raydir,
 
 void Raytracer_int::render(unsigned int* imageData) {
 	Color pixel;
-	float invWidth = 1 / float(screenWidth), invHeight = 1 / float(screenHeight);
-	float fov = 30, aspectratio = screenWidth / float(screenHeight);
-	float angle = tan(M_PI * 0.5 * fov / 180.);
+	int invWidth_2x = (FP_ONE << 1) / screenWidth;
+	int invHeight_2x = (FP_ONE << 1) / screenHeight;
+	int aspectratio = (screenWidth << FP_PRECISION) / screenHeight;
+	int angle = (unsigned int) (0.267949194 * FP_ONE);
 	// Trace rays
 	for (unsigned y = 0; y < screenHeight; ++y) {
 		for (unsigned x = 0; x < screenWidth; ++x) {
-			float xx = (2 * ((x + 0.5) * invWidth) - 1) * angle * aspectratio;
-			float yy = (1 - 2 * ((y + 0.5) * invHeight)) * angle;
-			Vec3f raydir = { xx, yy, -1 };
+			int xx = (((int) ((((((x << FP_PRECISION) + (FP_ONE >> 1))
+					* (long) invWidth_2x) >> FP_PRECISION) - FP_ONE) * angle)
+					>> FP_PRECISION) * aspectratio) >> FP_PRECISION;
+			int yy = ((FP_ONE
+					- ((((y << FP_PRECISION) + (FP_ONE >> 1)) * (long) invHeight_2x)
+							>> FP_PRECISION)) * angle) >> FP_PRECISION;
+			Vec3i raydir = { xx, yy, -FP_ONE };
 			normalize(&raydir);
-			Vec3i raydir_i = generateVectorI(raydir.x * FP_ONE, raydir.y * FP_ONE,
-					raydir.z * FP_ONE);  //TODO calulation before in int!!
-			pixel = trace(generateVectorI(0, 0, 0), raydir_i, 0);
+			pixel = trace(generateVectorI(0, 0, 0), raydir, 0);
 			*imageData++ = (std::min((unsigned int) 255, pixel.r)) << 16
 					| (std::min((unsigned int) 255, pixel.g)) << 8
 					| (std::min((unsigned int) 255, pixel.b));
